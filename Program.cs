7115796900370b4739ee00bd8b002779c76c392e
Nahdaa <jnahdaa@gmail.com>
2023-05-07 20:18:29 +0100

    Script for stopwatch program.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Loops until the user quits the program, so the stopwatch can be used repeatedly.
+            while (true)
+            {
+                // Providing options for the user.
+                Console.WriteLine("Start Stopwatch (s), Quit (q)");
+                var userInput = Console.ReadLine().ToLower();
+
+                // Creating an instance of the Stopwatch class.
+                var stopWatch = new Stopwatch();
+
+                if (userInput == "s")
+                {
+                    // Sets the current time as the stopwatch start time.
+                    stopWatch.Start(userInput);
+                    Console.WriteLine("Stop Stopwatch (x)"); // User can now stop the stopwatch.
+                    userInput = Console.ReadLine();
+
+                    if (userInput == "x" || userInput == "s")
+                    {
+                        // Sets current time as the stopwatch stop time.
+                        stopWatch.Stop(userInput);
+                        Console.WriteLine("Duration = "+ stopWatch.Duration()+"\n"); // Displays the duration.
+                    }
+                    else { Console.WriteLine("Invalid Input. Routing back to main menu."); } // If anything other than x is input.
+
+                }
+                else if (userInput == "q") // If the user wants to quit, the program stops.
+                {
+                    break;
+                }
+                else { Console.WriteLine("Invalid Input."); }
+
+            }
+        }
+    }
+}
---
+++ Stopwatch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch
+{
+    internal class Stopwatch
+    {
+        // Initialising the stopwatch start and stop time.
+        private TimeSpan _startTime;
+        private TimeSpan _stopTime;
+
+        public void Start(string startButton) // Method to 'start' the stopwatch.
+        {
+            this._startTime = DateTime.Now.TimeOfDay;
+        }
+
+        public void Stop(string stopButton) // Method to 'stop' the stopwatch.
+        {
+            if (stopButton == "s")
+            {
+                // If the user attempts to start the stopwatch when it is already running, program throws an exception.
+                throw new InvalidOperationException("Cannot start stopwatch after it has already been started.");
+            }
+
+            this._stopTime = DateTime.Now.TimeOfDay;
+
+        }
+
+        public TimeSpan Duration() // Calculating the duration.
+        {
+            return this._stopTime - this._startTime;
+        }
+    }
+}
